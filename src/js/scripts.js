c778b07b489f6b7bd461da10c0aa09efa60dee47
matheuspython <ruivorobado@gmail.com>
2022-03-21 17:46:55 -0300

    preparando o js

---
+++ src/js/scripts.js
@@ -0,0 +1,19 @@
+const fields = document.querySelectorAll('.fields')
+
+
+
+fields.forEach(el => {
+  el.addEventListener('click', e =>{
+    
+    
+  })
+})
+
+function vezDoPlayer(){
+
+}
+
+function game(){
+
+
+}
